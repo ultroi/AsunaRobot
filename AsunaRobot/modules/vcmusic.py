7b5fb1f62b7ad130ce214ca254bf33672a189db1
ultroi <86416271+ultroi@users.noreply.github.com>
2021-11-24 19:57:01 +0530

    Add files via upload

---
+++ AsunaRobot/modules/vcmusic.py
@@ -0,0 +1,54 @@
+__mod_name__ = "VC Music"
+
+__help__ = """
+          *》PIKACHU VC PLAYER《*
+            
+*I can now also play music in your group's voice chat*
+ 
+*Assistant username*:- @PikachuVcAssistant
+ 
+ *Setting up 
+1) Make bot admin
+2) Start a voice chat
+3) Try /play [song name] for the first time by an admin
+) If userbot joined enjoy music, If not add* @PikachuVcAssistant to your group manually and retry
+ 
+ *Commands* :-
+• Song Playing 🎶
+  `/play <songname>`: *Play song using youtube music*
+  `/play <yt url>` : *Play the given yt url*
+  `/splay`: *Play song via jio saavn*
+  
+• *Playback ⏯
+  /player: Open Settings menu of player
+  /skip: Skips the current track
+  /pause: Pause track
+  /resume: Resumes the paused track
+  /end: Stops media playback
+  /current: Shows the current Playing track
+  /playlist: Shows playlist*
+
+• *More tools* 📲
+`/musicplayer : <on/off>` *you can off ya on music*
+`/admincache`: *Updates admin info of your group. Try if bot isn't recognize admin*
+`/userbotjoin`: *Invite @PikachuVcAssistant to your chat*
+*Player cmd and all other cmds except /play, /current and /playlist are only for admins with manage group*
+
+          >Stream•Player<-
+
+=>Stream Playing
+❍ /vplay - play a vedio request 
+
+
+=>> Admins Only 
+❍ /streamassistantjoin - invite assistnt to play stream 
+❍ /streamassistantleave - to leave assistant 
+❍ /skip - to skip the streaming 
+❍ /stop - to stop the streaming 
+❍ /pause - to pause the stream 
+❍ /resume - to resume the stream 
+❍ /vmute - to mute the Userbot 
+❍ /vunmute - to unmute the userbot 
+
+x Assistant - @CharizardStreamAssistant
+"""
