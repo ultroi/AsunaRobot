dd37b610335849d5061d5b28eb923d17951913ea
ultroi <86416271+ultroi@users.noreply.github.com>
2021-11-25 06:26:24 +0530

    Update __tools.py

--- AsunaRobot/modules/__tools.py
+++ AsunaRobot/modules/__tools.py
@@ -2,14 +2,18 @@ __mod_name__ = "◎Tools"
 
 __help__ = """
 *Telegraph*
- ❍ /txm :Get Telegraph Link Of Replied Media
- ❍ /txt :Get Telegraph Link of Replied Text
+ ❍ /txm or /tm :Get Telegraph Link Of Replied Media
+ ❍ /txt or /tt :Get Telegraph Link of Replied Text
 
-*Chatbot* 
-_Admins only:_
- ❍ /addchat*:* Enables Chatbot mode in the chat.
- ❍ /rmchat*:* Disables Chatbot mode in the chat.
-
+*Translator* 
+_Both Parties:_
+ ❍ /tl (or /tr): as a reply to a message, translates it to English.
+ ❍ /tl <lang>: translates to <lang>
+    eg:`/tl ja`: translates to Japanese.
+ ❍ /tl <source>//<dest>: translates from <source> to <lang>.
+    eg: `/tl ja//en`: translates from Japanese to English.
+ ❍ /langs: get a list of supported languages for translation.
+ 
 *Date-time-Weather*
  ❍ /time <country code>*:* Gives information about a timezone.
  ❍ /weather <city>*:* Get weather info in a particular place.
@@ -20,5 +24,11 @@ _Admins only:_
  ❍ /encrypt*:* Encrypts The Given Text
  ❍ /decrypt*:* Decrypts Previously Ecrypted Text
  ❍ /zip*:* reply to a telegram file to compress it in .zip format
- ❍ /unzip*:* reply to a telegram file to decompress it from the .zip format
+ ❍ /unzip*:* reply to a telegram file to decompress it from the .zip format 
+ 
+*TTS/STT*
+ I can convert text to voice and voice to text.
+ ❍ /tts <`lang code`>: Reply to any message to get text to speech output 
+ ❍ /stt: Type in reply to a voice message`(support english only)` to extract text from it.
+  [Language Codes](buttonurl://https://telegra.ph/Language-Codes-11-25)
 """
