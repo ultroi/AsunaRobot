9b7291b276f57682e3dfe20ec34185998fcc7b27
ultroi <86416271+ultroi@users.noreply.github.com>
2021-11-27 20:29:53 +0530

    Update __main__.py

--- AsunaRobot/__main__.py
+++ AsunaRobot/__main__.py
@@ -75,7 +75,7 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-────「 [{}](https://telegra.ph/file/da8c9c0c573d0cd2a5913.jpg) 」────
+────「 [{Pikachu}](https://telegra.ph/file/da8c9c0c573d0cd2a5913.jpg) 」────
 *Hola! {},*
 *I am an Anime themed advance group management bot with a lot of Features.*
 ➖➖➖➖➖➖➖➖➖➖➖➖➖
