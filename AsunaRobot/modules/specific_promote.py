830512645f1ed91138ec3e818ec5c225951beac3
ultroi <86416271+ultroi@users.noreply.github.com>
2021-12-03 17:22:12 +0530

    Update specific_promote.py

--- AsunaRobot/modules/specific_promote.py
+++ AsunaRobot/modules/specific_promote.py
@@ -1,4 +1,29 @@
+import html 
+from telegram import Prasemode, Update 
+from telegram.error import BadRequest  
+from telegram.ext import CallbackContext, CommandHandler, Filters, run_async 
+from telegram.utils.helpers import mention_html 
+from AsunaRobot import DRAGONS, dispatcher 
+from AsunaRobot.modules.disable import DisableAbleCommandHandler 
+from AsunaRobot.modules.helper_funcs.chat_status import ( 
+    bot_admin, 
+    can_pin, 
+    can_promote, 
+    connection_status, 
+    user_admin, 
+    ADMIN_CACHE, 
+) 
+from AsunaRobot.modules.helper_funcs.extraction import ( 
+    extract_user, 
+    extract_user_and_text, 
+)
+from AsunaRobot.modules.log_channel import loggable 
+from AsunaRobot.modules.helper_funcs.alternate import send_message
 
+
+
+
+ 
 @connection_status
 @bot_admin
 @can_promote
