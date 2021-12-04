c1400fbe827692bde6092be0ccb04a0b4abd7284
ultroi <86416271+ultroi@users.noreply.github.com>
2021-12-04 15:44:11 +0530

    Update bans.py

--- AsunaRobot/modules/bans.py
+++ AsunaRobot/modules/bans.py
@@ -420,7 +420,7 @@ dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(PUNCHME_HANDLER)
 
-__mod_name__ = "Bans"
+__mod_name__ = "Ban/Mute"
 __handlers__ = [
     BAN_HANDLER,
     TEMPBAN_HANDLER,
