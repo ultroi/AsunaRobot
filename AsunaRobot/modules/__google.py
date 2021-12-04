383810fb958e944b45821f17c850921124796400
ultroi <86416271+ultroi@users.noreply.github.com>
2021-12-04 17:20:41 +0530

    Update __google.py

--- AsunaRobot/modules/__google.py
+++ AsunaRobot/modules/__google.py
@@ -276,7 +276,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "◎Search"
+__mod_name__ = "Google"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
