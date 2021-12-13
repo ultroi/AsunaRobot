af4da042690dc13daf219f1b5da1bf801c7029f3
ultroi <86416271+ultroi@users.noreply.github.com>
2021-12-13 18:41:19 +0530

    Update sangmata.py

--- AsunaRobot/modules/sangmata.py
+++ AsunaRobot/modules/sangmata.py
@@ -3,7 +3,7 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl import functions, types
 
 from AsunaRobot.events import register as kurkur
-from AsunaRobot import telethn, ubot2 as ubot
+from AsunaRobot import telethn, ubot
 
 
 async def is_register_admin(chat, user):
