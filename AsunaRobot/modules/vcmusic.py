c800e64371c2f7a28da02fc070add7659c96cca6
ultroi <86416271+ultroi@users.noreply.github.com>
2021-12-18 11:00:52 +0530

    Create vcmusic.py

---
+++ AsunaRobot/modules/vcmusic.py
@@ -0,0 +1,30 @@
+__mod_name__ = "VC Music"
+
+__help__ = """
+
+               *Music*
+
+•*Basic Cmd*
+» /play (song name/link) - play music on video chat
+» /vplay (video name/link) - play video on video chat
+» /vstream - play live video from yt live/m3u8
+» /playlist - show you the playlist
+» /video (query) - download video from youtube
+» /song (query) - download song from youtube
+» /lyric (query) - scrap the song lyric
+» /search (query) - search a youtube video link
+
+•*Admin Cmd*
+» /pause - pause the stream
+» /resume - resume the stream
+» /skip - switch to next stream
+» /stop - stop the streaming
+» /vmute - mute the userbot on voice chat
+» /vunmute - unmute the userbot on voice chat
+» /volume 1-200 - adjust the volume of music (userbot must be admin)
+» /reload - reload bot and refresh the admin data
+» /userbotjoin - invite the userbot to join group
+» /userbotleave - order userbot to leave from group
+
+x Assistant - @CharizardStreamAssistant
+"""
