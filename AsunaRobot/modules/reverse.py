b96ab4632034a774911584ce9f467d379293c482
ultroi <86416271+ultroi@users.noreply.github.com>
2022-01-18 21:11:38 +0530

    Update reverse.py

--- AsunaRobot/modules/reverse.py
+++ AsunaRobot/modules/reverse.py
@@ -181,7 +181,7 @@ def scam(imgspage, lim):
 __mod_name__ = "Reverse"
 
 REVERSE_HANDLER = DisableAbleCommandHandler(
-    ("reverse", "grs"), reverse, pass_args=True, admin_ok=True
+    "reverse", reverse, pass_args=True, admin_ok=True
 )
 
 dispatcher.add_handler(REVERSE_HANDLER)
