f4564fd568555ef33a2de8bf0dc5020c82dd488c
ultroi <86416271+ultroi@users.noreply.github.com>
2022-01-29 09:54:06 +0530

    Update reverse.py

--- AsunaRobot/modules/reverse.py
+++ AsunaRobot/modules/reverse.py
@@ -19,7 +19,7 @@ useragent = 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebK
 opener.addheaders = [('User-agent', useragent)]
 
 @run_async
-@AsunaRobot(command=["grs", "reverse"])
+@Asuna(command=["grs", "reverse"])
 def reverse(update: Update, context:CallbackContext):
     if os.path.isfile("okgoogle.png"):
         os.remove("okgoogle.png")
