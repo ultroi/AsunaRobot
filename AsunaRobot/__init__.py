2a3b47e5c8955da8ae565d37b97ddd40a212f83f
ultroi <86416271+ultroi@users.noreply.github.com>
2022-02-14 10:17:14 +0530

    Update __init__.py

--- AsunaRobot/__init__.py
+++ AsunaRobot/__init__.py
@@ -232,7 +232,12 @@ print("[INFO]: INITIALIZING ARQ CLIENT")
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession) 
 ubot = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH) 
 print("[AsunaRobot]: Connecting To Yūki • Pikachu Userbot (t.me/Weeb_lover)")
-
+ubot2 = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
+try:
+    ubot2.start()
+except BaseException:
+    print("Userbot Error ! Have you added a STRING_SESSION in deploying??")
+    sys.exit(1)
 
 pbot = Client(
     ":memory:",
