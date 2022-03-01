c3c67878e9efb08740cb3536e751be77bbd02616
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-01 17:48:42 +0530

    Update global_bans.py

--- AsunaRobot/modules/global_bans.py
+++ AsunaRobot/modules/global_bans.py
@@ -29,16 +29,17 @@ from AsunaRobot import (
     sw,
     dispatcher,
 )
-from AsunaRobot.modules.helper_funcs.chat_status import (
+from SiestaRobot.modules.helper_funcs.chat_status import (
     is_user_admin,
     support_plus,
     user_admin,
 )
-from AsunaRobot.modules.helper_funcs.extraction import (
+from SiestaRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from AsunaRobot.modules.helper_funcs.misc import send_to_list
+from SiestaRobot.modules.helper_funcs.misc import send_to_list
+from SiestaRobot.modules.language import gs
 
 GBAN_ENFORCE_GROUP = 6
 
@@ -506,7 +507,7 @@ def gbanstat(update: Update, context: CallbackContext):
 
 
 def __stats__():
-    return f"• {sql.num_gbanned_users()} gbanned users."
+    return f"× {sql.num_gbanned_users()} gbanned users."
 
 
 def __user_info__(user_id):
@@ -537,20 +538,8 @@ def __chat_settings__(chat_id, user_id):
     return f"This chat is enforcing *gbans*: `{sql.does_chat_gban(chat_id)}`."
 
 
-__help__ = f"""
-*Admins only:*
- • `/antispam <on/off/yes/no>`*:* Will toggle our antispam tech or return your current settings.
-
-Anti-Spam, used by bot devs to ban spammers across all groups. This helps protect \
-you and your groups by removing spam flooders as quickly as possible.
-*Note:* Users can appeal gbans or report spammers at @{SUPPORT_CHAT}
-
-This also integrates @Spamwatch API to remove Spammers as much as possible from your chatroom!
-*What is SpamWatch?*
-SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/f584b643c6f4be0b1de53.jpg)
-Constantly help banning spammers off from your group automatically So, you wont have to worry about spammers storming your group.
-*Note:* Users can appeal spamwatch bans at @SpamwatchSupport
-"""
+def helps(chat):
+    return gs(chat, "antispam_help")
 
 GBAN_HANDLER = CommandHandler("gban", gban, run_async=True)
 UNGBAN_HANDLER = CommandHandler("ungban", ungban, run_async=True)
